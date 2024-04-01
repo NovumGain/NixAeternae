d799516c42be11835a08d1e5a3b4610abe2ca385
novum <isshouprism@gmail.com>
2024-04-01 21:59:17 +0000

    made some changes

--- nas/system/hotkey_settings.nix
+++ nas/system/hotkey_settings.nix
@@ -1,12 +1,9 @@
 { config, lib, pkgs, ... }:
 {
-
-  programs.light.enable = true; # Needed for the /run/wrappers/bin/light SUID wrapper.
   services.actkbd = {
     enable = true;
     bindings = [
       { keys = [ 71 ]; events = [ "KeyPress" ]; command = "/run/wrappers/bin/light -A 10"; }
     ];
   };
-
 }

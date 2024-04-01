65c6e188bf62ec089a0123805a8562e56268612f
novum <isshouprism@gmail.com>
2024-04-01 21:58:47 +0000

    made some changes

--- nas/system/hotkey_settings.nix
+++ nas/system/hotkey_settings.nix
@@ -5,8 +5,7 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
+      { keys = [ 71 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
     ];
   };
 
